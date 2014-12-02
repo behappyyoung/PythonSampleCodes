500cdd13f6d8be4301d3d593943fde25961b1eb7
young <behappydev@gmail.com>
2014-12-02 14:15:56 -0800

    update 2014-12-02-14:15:56 -

---
+++ Flask/helloworld.py
@@ -0,0 +1,15 @@
+# from http://flask.pocoo.org/ tutorial 
+from flask import Flask 
+app = Flask(__name__) 
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+if __name__ == "__main__": 
+    app.run()
+
