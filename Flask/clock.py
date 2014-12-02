0ed281a2473bb59477d310530e5e80d1323e1cc2
young <behappydev@gmail.com>
2014-12-02 14:19:43 -0800

    update 2014-12-02-14:19:43 -

--- Flask/clock.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask 
-# For this example we'll use SQLAlchemy, a popular ORM that supports a 
-# variety of backends including SQLite, MySQL, and PostgreSQL 
-from flask.ext.sqlalchemy import SQLAlchemy 
-app = Flask(__name__) 
-# We'll just use SQLite here so we don't need an external database 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' 
-db = SQLAlchemy(app)
