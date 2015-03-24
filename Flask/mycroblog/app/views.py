524467f61ec51a3b78a492cae29386a7e4df63f0
young <behappydev@gmail.com>
2015-03-24 09:18:12 -0700

    update 2015-03-24-09:18:09 -

--- Flask/mycroblog/app/views.py
+++ Flask/mycroblog/app/views.py
@@ -1,7 +1,11 @@
 from app import app
 
-
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/test')
+@app.route('/testing')
+def test():
+    return "Hello, Testing World!"
---
+++ Flask/projectR/appR/api/testapi.py
@@ -0,0 +1,20 @@
+__author__ = 'young'
+import pycurl
+from StringIO import StringIO
+
+def test_function(request):
+    print request.args
+    print request.__dict__
+    print request.args.__dict__
+
+    return 'test return'
+
+
+def curl_test(url):
+    buffer = StringIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, url)
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+    return buffer.getvalue()
