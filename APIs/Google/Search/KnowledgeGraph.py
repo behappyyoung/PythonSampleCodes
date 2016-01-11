8f72f1917595361708a41b53ba41957622b9327d
Young Park <young.p2@YOUNG-P-MBP.local>
2016-01-11 13:07:49 -0800

    01-11-2016_13:07:47

---
+++ APIs/Google/Search/KnowledgeGraph.py
@@ -0,0 +1,19 @@
+"""Example of Python client calling Knowledge Graph Search API."""
+import json
+import urllib
+
+api_key = open('.api_key').read()
+query = 'centrify'
+service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+params = {
+    'query': query,
+    'limit': 10,
+    'indent': True,
+    'key': api_key,
+}
+url = service_url + '?' + urllib.urlencode(params)
+response = json.loads(urllib.urlopen(url).read())
+print response
+for element in response['itemListElement']:
+  print element['result']['name'] + ' (' + str(element['resultScore']) + ')'
+
---
+++ SampleCodes/searchFile.py
@@ -0,0 +1,22 @@
+from sys import argv
+script, filename = argv
+import re
+
+dict={}
+
+with open(filename) as f:
+	
+	for line in f:
+		print line
+		m= re.search('add key="(/.*)" value="(.*)"', line)
+		if m:
+			print m.group(0)
+			m1 = m.group(1)
+			m2 = m.group(2)
+			if(m2!= 'y'):
+				print type(m1), m1, m2
+				dict[m1] = m2
+
+##print dict
+
+
