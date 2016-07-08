eb0689b9dd6c6f691ee0057bda4c74d928da6f56
Young Park <young_s_p@hotmail.com>
2016-07-08 00:15:28 -0700

    2016-07-08

--- Django/3/projectE/shipment/views.py
+++ Django/3/projectE/shipment/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import Http404
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
+from django.core import serializers
 
 from shipment.models import Item, Job, 거래처, 현장, 출하
 
@@ -34,7 +35,7 @@ def user_login(request):
         # Use Django's machinery to attempt to see if the username/password
         # combination is valid - a User object is returned if it is.
         user = authenticate(username=username, password=password)
-
+        print('login', username, password, user)
         # If we have a User object, the details are correct.
         # If None (Python's way of representing the absence of a value), no user
         # with matching credentials was found.
@@ -44,9 +45,11 @@ def user_login(request):
                 # If the account is valid and active, we can log the user in.
                 # We'll send the user back to the homepage.
                 login(request, user)
+                request.session['username'] = username
                 shipments = 출하.objects.all()
-                print(shipments)
-                return render(request, 'shipment/index.html', {'shipments': shipments})
+                request.session['shipments'] = serializers.serialize('json', shipments)
+                print('shipments', shipments)
+                return render(request, 'shipment/index.html', {})
             else:
                 # An inactive account was used - no logging in!
                 return HttpResponse("Your  account is disabled.")
@@ -61,3 +64,10 @@ def user_login(request):
         # No context variables to pass to the template system, hence the
         # blank dictionary object...
         return render(request, 'shipment/login.html', {})
+
+def logout(request):
+    try:
+        del request.session['username']
+    except KeyError:
+        pass
+    return HttpResponse("You're logged out.")
