01a4f774168e3b34ac061cf9f161e723c9249e81
Young Park <young_s_p@hotmail.com>
2016-07-14 01:32:15 -0700

    2016-07-14

--- Django/3/projectE/shipment/models.py
+++ Django/3/projectE/shipment/models.py
@@ -10,19 +10,40 @@ class Job(models.Model):
     번호 =  models.CharField(max_length=200)
     예정량 =  models.IntegerField()
 
+class 규격명(models.Model):
+    m40= models.CharField(max_length=200, default='40mm')
+    m25= models.CharField(max_length=200, default='25mm')
+    m19 = models.CharField(max_length=200, default='19mm')
+    m13 = models.CharField(max_length=200, default='13mm')
+
+class 규격(models.Model):
+    규격명=models.CharField(max_length=200, default=' ')
+    단가=models.IntegerField()
+    운반비=models.IntegerField()
+
+class 차량(models.Model):
+    차량번호=models.CharField(max_length=200, default=' ')
+    기사이름=models.CharField(max_length=200, default=' ')
+    기사연락처=models.CharField(max_length=200, default=' ')
+    적재량=models.IntegerField()
+
 class 현장(models.Model):
     현장명=models.CharField(max_length=200, default=' ')
+    규격=models.ForeignKey('규격', on_delete=models.CASCADE, default=00)
+
     
 class 거래처(models.Model):
     코드= models.CharField(max_length=200, default='0000')
     거래처명=models.CharField(max_length=200)
     대표자 = models.CharField(max_length=200)
     주소 = models.CharField(max_length=200)
-    연락처1 = models.CharField(max_length=200)
-    연락처2 = models.CharField(max_length=200)
-    연락처3 = models.CharField(max_length=200)
+    전화 = models.CharField(max_length=200)
+    팩스 = models.CharField(max_length=200)
+    담당자 = models.CharField(max_length=200)
     email=models.CharField(max_length=200)
     현장 = models.ForeignKey('현장', on_delete=models.CASCADE, default=00)
+    규격 = models.ForeignKey('규격', on_delete=models.CASCADE, default=00)
+    차량 = models.ForeignKey('차량', on_delete=models.CASCADE, default=00)
 
     class Meta:
         ordering = ['거래처명']
