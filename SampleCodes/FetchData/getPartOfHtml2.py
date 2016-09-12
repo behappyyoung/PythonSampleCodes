33a84ccf3df2d69a82a4fcb7f3b79842abaf6c7d
young park <ypark@stanfordhealthcare.org>
2016-09-12 09:51:13 -0700

    2016-09-12 09:51:12 AM

---
+++ SampleCodes/FetchData/getPartOfHtml2.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'http://med.stanford.edu/'
+
+# using requests
+try:
+    response = requests.get(url)
+    message = response.content
+    soup = BeautifulSoup(message, 'html.parser')
+    mydiv = str(soup.find("div", class_="panel-builder-100-col"))
+    file_ = open('pulseDiv', 'w')
+    file_.write(mydiv)
+    file_.close()
+except requests.exceptions.RequestException as e:  # This is the correct syntax
+    print (e)
+    message = "could not fetch %s" % url
+    print (message)
+    file_ = open('pulseDiv', 'r')
+    mydiv = file_.read()
+    file_.close()
+
+print(mydiv)
+
