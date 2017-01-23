519314d0879c463386c90de404919ecb64367f1f
young park <behappy.young@gmail.com>
2017-01-23 09:02:20 -0800

    2017-01-23 09:02:16 AM

---
+++ Algorithm/HackerRank/Introductions/Swap-Case.py
@@ -0,0 +1,11 @@
+'''
+Sample Input
+
+HackerRank.com presents "Pythonist 2".
+Sample Output
+
+hACKERrANK.COM PRESENTS "pYTHONIST 2".
+
+'''
+
+S = raw_input().strip()
--- Algorithm/HackerRank/Love-Letter.py
+++ Algorithm/HackerRank/Love-Letter.py
@@ -8,9 +8,10 @@ He can reduce the value of a letter, e.g. he can change d to c, but he cannot ch
 In order to form a palindrome, if he has to repeatedly reduce the value of a letter, he can do it until the letter becomes a. Once a letter has been changed to a, it can no longer be changed.
 Each reduction in the value of any letter is counted as a single operation. Find the minimum number of operations required to convert a given string into a palindrome.
 '''
-import socket
-
 import math
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
 
 T = int(raw_input())
 myinput =[]
@@ -25,55 +26,7 @@ def makePalindrome(intArray):
     j = length -1
 
     while i <= math.ceil(length/2.0)-1:
-        if i==j or j-i==1:
-            break
-	if i > 10:
-		break
         gonext = 'stay'
-<<<<<<< HEAD
-##        print i, j, ' :' , stepcount, '  ', intArray
-        if intArray[i] == intArray[j]:                               ## should be  [i] == [j] always
-            idiff = int(intArray[i+1]-intArray[i])
-            jdiff = int(intArray[j-1]-intArray[j])
-	    print i, j, intArray[i], intArray[i+1], intArray[j-1], intArray[j], idiff, jdiff, stepcount
-	    if idiff ==1 and jdiff ==1:
-                gonext = 'next'
-
-            elif 1 < idiff:
-                intArray[i+1] = intArray[i+1] - ( idiff - 1 )
-                stepcount = stepcount + ( idiff - 1 )
-            elif 1 < jdiff:
-                intArray[j-1] = intArray[j-1] - ( jdiff - 1)
-                stepcount = stepcount +  ( jdiff - 1 )
-
-	    elif 0 > idiff:
-	            intArray[i] = intArray[i]+idiff
-	            stepcount = stepcount -idiff
-
-            elif 0 > jdiff:
-	                intArray[j] = intArray[j] +jdiff
-	                stepcount = stepcount  - jdiff
-
-	    elif 0== idiff:
-		    if i!=0 and intArray[i] >= 98 :
-			intArray[i] = intArray[i]  -1
-	                intArray[j] = intArray[j] -1
-	                stepcount = stepcount  + 2
-                    	gonext = 'prev'
-		    else:
-			gonext = 'next'	
-
-            elif 0== jdiff:
-		    if i!=0 and intArray[i] >= 98 :
-			intArray[i] = intArray[i]  -1
-	                intArray[j] = intArray[j] -1
-	                stepcount = stepcount  + 2
-                    	gonext = 'prev'
-		    else:
-			gonext = 'next'	
-                
-           
-=======
         if (i !=0) and (  i==j or j-i==1) :
                 break
 
@@ -89,7 +42,6 @@ def makePalindrome(intArray):
                     stepcount = stepcount + diff
             else:
                 gonext = 'next'
->>>>>>> 42a57001c2336187c4b6f08870f341f7cc6cabaf
 
         elif intArray[i]<intArray[j]:
             diff = intArray[j]-intArray[i]
@@ -115,4 +67,5 @@ for i in myinput:
         intArray.append(int(ord(c)))
 
     print makePalindrome(intArray)
+
 
