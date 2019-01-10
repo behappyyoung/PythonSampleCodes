73bb135730a7fe6bf31001a27bb77ef7ff68d93c
Young Park <behappyyoung@gmail.com>
2019-01-10 11:17:21 -0800

    2019-01-10 11:17:21 AM

---
+++ SampleCodes/Library/pdfkit/saveTopdf.py
@@ -0,0 +1,2 @@
+import pdfkit
+pdfkit.from_url('http://127.0.0.1:8000', 'files/pdfkit.pdf')
--- SampleCodes/sendmail.py
+++ SampleCodes/sendmail.py
@@ -3,7 +3,7 @@ from subprocess import Popen, PIPE
 
 msg = MIMEText("Here is the body of my message")
 msg["From"] = "young.park"
-msg["To"] = "behappycentrify@gmail.com"
+msg["To"] = "youngspark@outlook.com"
 msg["Subject"] = "Testing Result"
 p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
 p.communicate(msg.as_string())
